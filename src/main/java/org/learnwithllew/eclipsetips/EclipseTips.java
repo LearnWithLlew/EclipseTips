45d27be2c572809ae5120bf43d3e1aceeea480ea
Llewellyn Falco <isidore@setgame.com>
2016-06-01 13:13:09 +0300

    Initial Commit

---
+++ src/main/java/org/learnwithllew/eclipsetips/EclipseTips.java
@@ -0,0 +1,83 @@
+package org.learnwithllew.eclipsetips;
+
+public class EclipseTips
+{
+  /**
+   * <table>
+   *  <tr> <th colspan="2"> Lines </tr>
+   *  <tr>
+   *   <th>Command</th>
+   *   <th>ShortCut</th>
+   *  </tr>
+   *  <tr  bgcolor="lightgray">
+   *   <td> Delete Line </td>
+   *   <td> Ctrl+D
+   *  </tr>
+   *  <tr  bgcolor="darkgrey">
+   *   <td> Duplicate Line </td>
+   *   <td> Ctrl + Alt + &#8679;
+   *  </tr>
+   *  <tr  bgcolor="lightgray">
+   *   <td> Move Line </td>
+   *   <td> Alt + &#8679;
+   *  </tr>
+   *  <tr> <th colspan="2"> </br>Refactorings (Ctrl+Alt) </tr>
+   *  <tr>
+   *   <th>Command</th>
+   *   <th>ShortCut</th>
+   *  </tr>
+   *  <tr  bgcolor="darkgrey">
+   *   <td> Extract (L)ocal Variable </td>
+   *   <td> Ctrl + Alt + L
+   *  </tr>
+   *  <tr  bgcolor="lightgray">
+   *   <td> (I)nline </td>
+   *   <td> Ctrl + Alt + I
+   *  </tr>
+   *  <tr  bgcolor="darkgrey">
+   *   <td> Extract (M)ethod </td>
+   *   <td> Ctrl + Alt + M
+   *  </tr>
+   *  <tr  bgcolor="lightgray">
+   *   <td> (R)ename </td>
+   *   <td> Ctrl + Alt + R
+   *  </tr>
+   *  <tr> <th colspan="2"> </br>Completions (Ctrl+Alt) </tr>
+   *  <tr>
+   *   <th>Command</th>
+   *   <th>ShortCut</th>
+   *  </tr>
+   *  <tr  bgcolor="darkgrey">
+   *   <td> Auto Complete  </td>
+   *   <td> Ctrl + space
+   *  </tr>
+   *  <tr  bgcolor="lightgray">
+   *   <td> QuickFix</td>
+   *   <td> Ctrl + 1
+   *  </tr>
+   *  <tr  bgcolor="darkgrey">
+   *   <td> Search Help</td>
+   *   <td> Ctrl + 3
+   *  </tr>
+   *  <tr> <th colspan="2"> </br>Running (Ctrl+Alt) </tr>
+   *  <tr>
+   *   <th>Command</th>
+   *   <th>ShortCut</th>
+   *  </tr>
+   *  <tr  bgcolor="darkgrey">
+   *   <td> Run  </td>
+   *   <td> Ctrl + F11
+   *  </tr>
+   *  <tr  bgcolor="lightgray">
+   *   <td> Debug</td>
+   *   <td> Ctrl + Shift + F11
+   *  </tr>
+   *  
+   * </table>
+   *
+   */
+  public static WindowsShortCuts Windows()
+  {
+    return null;
+  }
+}
