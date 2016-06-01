812b29816bc0ac11d313480afcbe3c33cbe86151
Llewellyn Falco <isidore@setgame.com>
2016-06-01 15:32:52 +0300

    screen shot

---
+++ src/main/java/org/learnwithllew/eclipsetips/WindowsShortCuts.java
@@ -0,0 +1,105 @@
+package org.learnwithllew.eclipsetips;
+
+public interface WindowsShortCuts
+{
+ /**
+   * <table>
+   *  <tr> <th colspan="2"> Completions </tr>
+   *  <tr>
+   *   <th>Command</th>
+   *   <th>ShortCut</th>
+   *  </tr>
+      *  <tr  bgcolor="lightgray">
+   *   <td> Auto Complete </td>
+   *   <td> Ctrl + space </td>
+   *  </tr>
+      *  <tr  bgcolor="darkgrey">
+   *   <td> QuickFix </td>
+   *   <td> Ctrl + 1 </td>
+   *  </tr>
+      *  <tr  bgcolor="lightgray">
+   *   <td> Search Help </td>
+   *   <td> Ctrl + 3 </td>
+   *  </tr>
+      *  <tr  bgcolor="darkgrey">
+   *   <td> Generate (S)ource </td>
+   *   <td> Alt + Shift + S </td>
+   *  </tr>
+      * </table>
+  **/
+    public void _Completions();
+ /**
+   * <table>
+   *  <tr> <th colspan="2"> Running </tr>
+   *  <tr>
+   *   <th>Command</th>
+   *   <th>ShortCut</th>
+   *  </tr>
+      *  <tr  bgcolor="lightgray">
+   *   <td> Run </td>
+   *   <td> Ctrl + F11 </td>
+   *  </tr>
+      *  <tr  bgcolor="darkgrey">
+   *   <td> Debug </td>
+   *   <td> F11 </td>
+   *  </tr>
+      * </table>
+  **/
+    public void _Running();
+ /**
+   * <table>
+   *  <tr> <th colspan="2"> Refactorings </tr>
+   *  <tr>
+   *   <th>Command</th>
+   *   <th>ShortCut</th>
+   *  </tr>
+      *  <tr  bgcolor="lightgray">
+   *   <td> Extract (L)ocal Variable </td>
+   *   <td> Alt + Shift + L </td>
+   *  </tr>
+      *  <tr  bgcolor="darkgrey">
+   *   <td> Extract (M)ethod </td>
+   *   <td> Alt + Shift + M </td>
+   *  </tr>
+      *  <tr  bgcolor="lightgray">
+   *   <td> (C)hange Method Signature </td>
+   *   <td> Alt + Shift + C </td>
+   *  </tr>
+      *  <tr  bgcolor="darkgrey">
+   *   <td> (R)ename </td>
+   *   <td> Alt + Shift + R </td>
+   *  </tr>
+      *  <tr  bgcolor="lightgray">
+   *   <td> (I)nline </td>
+   *   <td> Alt + Shift + I </td>
+   *  </tr>
+      * </table>
+  **/
+    public void _Refactorings();
+ /**
+   * <table>
+   *  <tr> <th colspan="2"> Lines </tr>
+   *  <tr>
+   *   <th>Command</th>
+   *   <th>ShortCut</th>
+   *  </tr>
+      *  <tr  bgcolor="lightgray">
+   *   <td> Delete Line </td>
+   *   <td> Ctrl + D </td>
+   *  </tr>
+      *  <tr  bgcolor="darkgrey">
+   *   <td> Grow Selection </td>
+   *   <td> Alt + Shift + &#11014;/&#11015; </td>
+   *  </tr>
+      *  <tr  bgcolor="lightgray">
+   *   <td> Duplicate Line </td>
+   *   <td> Ctrl + Alt + &#11014;/&#11015; </td>
+   *  </tr>
+      *  <tr  bgcolor="darkgrey">
+   *   <td> Move Line </td>
+   *   <td> Alt + &#11014;/&#11015; </td>
+   *  </tr>
+      * </table>
+  **/
+    public void _Lines();
+}
